63200cafb86d679457ca3e2a7ed3c0599c0e72b2
VIVEK BHADORIYA <viveksinghbhadoriya4812@gmail.com>
2026-07-07 09:30:07 +0530

    cors resolve

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 // Security and Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'http://localhost:5177'], // Allow multiple Vite ports
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'http://localhost:5177', 'https://erp-skillyon-student.vercel.app/', 'https://erp-skillyon-pare.vercel.app/'], // Allow multiple Vite ports
   credentials: true
 }));
 app.use(express.json());
